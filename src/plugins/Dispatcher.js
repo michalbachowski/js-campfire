8672aebfa7b47f11990f57977ac1851d3801b2d1
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 14:14:27 +0200

    JSLint compliance

--- src/plugins/Dispatcher.js
+++ src/plugins/Dispatcher.js
@@ -1,24 +1,39 @@
-function ChatPluginDispatcher() {
-    Listener.apply(this, arguments);
+var ChatPluginDispatcher = (function (Listener, Event) {
+    "use strict";
+    return function () {
+        Listener.apply(this, arguments);
 
-    this.mapping = function() {
-        return {
-            "chat.message.dispatch": dispatch
+        var self = this,
+            dispatch = function (event) {
+                // HOOK: filter massage
+                var message = self.dispatcher.filter(
+                        new Event(
+                            self,
+                            "dispatcher.message.filter",
+                            {}
+                        ),
+                        event.parameter("message")
+                    ).getReturnValue(),
+                    // HOOK: display
+                    evt = self.dispatcher.notifyUntil(
+                        new Event(
+                            self,
+                            "dispatcher.message.display",
+                            {message: message}
+                        )
+                    );
+                if (!evt.isProcessed()) {
+                    return;
+                }
+                // HOOK: message displayed
+                self.dispatcher.notify(new Event(self, "dispatcher.message.displayed", {message: message}));
+                return true;
+            };
+
+        this.mapping = function () {
+            return {
+                "chat.message.dispatch": dispatch
+            };
         };
     };
-
-    var self = this;
-
-    var dispatch = function(event) {
-        // HOOK: filter massage
-        var message = self.dispatcher.filter(new Event(self, "dispatcher.message.filter", {}), event.parameter("message")).getReturnValue();
-        // HOOK: display
-        var evt = self.dispatcher.notifyUntil(new Event(self, "dispatcher.message.display", {message: message}));
-        if ( !evt.isProcessed() ) {
-            return;
-        }
-        // HOOK: message displayed
-        self.dispatcher.notify(new Event(self, "dispatcher.message.displayed", {message: message}));
-        return true;
-    };
-}
+}(Listener, Event));
