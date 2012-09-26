dc57ca0d5ecfba739f0388167b3aa81a2d66d79f
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 14:55:26 +0200

    JSLint compliance

--- src/plugins/MessageTime.js
+++ src/plugins/MessageTime.js
@@ -1,30 +1,32 @@
-function ChatPluginMessageTime() {
-    Listener.apply( this, arguments );
-    
-    this.mapping = function() {
-        return {
-            'display_message.node.filter': notify
+var ChatPluginMessageTime = function (jQuery, Listener) {
+    "use strict";
+    return function () {
+        Listener.apply(this, arguments);
+        
+        var fullTime = function (date) {
+                if (date <= 9) {
+                    return "0" + date;
+                }
+                return date;
+            },
+
+            notify = function (event, node) {
+                var date = new Date();
+                date.setTime(event.parameter('message').date * 1000);
+                node.prepend(
+                    jQuery("<small />")
+                        .append(fullTime(date.getHours()))
+                        .append(":")
+                        .append(fullTime(date.getMinutes()))
+                        .append(" ")
+                );
+                return node;
+            };
+
+        this.mapping = function () {
+            return {
+                'display_message.node.filter': notify
+            };
         };
     };
-
-    var fullTime = function(date) {
-        if (date<=9) {
-            return "0" + date;
-        }
-        return date;
-    };
-
-    var notify = function(event, node) {
-        var date = new Date();
-        date.setTime( event.parameter('message').date * 1000 );
-        node.prepend(
-            jQuery( "<small />" )
-            .append( fullTime( date.getHours() ) )
-            .append( ":" )
-            .append( fullTime( date.getMinutes() ) )
-            .append( " " )
-        );
-        return node;
-    };
-
-}
+}(jQuery, Listener);
