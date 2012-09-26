ae83256dd03b2ffdd7f8ca5d68558c8c4ab64473
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 10:49:33 +0200

    JSLint compliance; Expect reference to InfoBox as input argument

--- src/plugins/InfoBox.js
+++ src/plugins/InfoBox.js
@@ -1,26 +1,20 @@
-function ChatPluginInfoBox( params ) {
-    Listener.apply( this, arguments );
+function ChatPluginInfoBox(box) {
+    "use strict";
+    Listener.apply(this, arguments);
     
-    this.mapping = function() {
+    var self = this,
+        display = function (event) {
+            box
+                .dialog("close")
+                .text(event.parameter("message"))
+                .dialog("open");
+            return true;
+        };
+
+    this.mapping = function () {
         return {
-            "chat.init": init,
             "info_box.display": display
         };
     };
 
-    var self = this;
-    var box;
-
-    var init = function(event) {
-        box = $("<p />").dialog({
-            autoOpen: false,
-            width: 200,
-            height: 100
-        });
-    };
-
-    var display = function(event) {
-        box.dialog("close").text(event.parameter("message")).dialog("open");
-        return true;
-    };
 }
