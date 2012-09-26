fd580b1d41975b50cfdfaabe57c585b3075fde8c
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 14:45:48 +0200

    JSLint compliance

--- src/plugins/Form.js
+++ src/plugins/Form.js
@@ -1,68 +1,91 @@
-function ChatPluginForm(formId) {
-    Listener.apply(this, arguments);
+var ChatPluginForm = (function (jQuery, Listener, Event) {
+    "use strict";
+    return function (formId) {
+        Listener.apply(this, arguments);
 
-    this.mapping = function() {
-        return {
-            "form.get": getForm
-        };
-    };
+        var self = this,
+            form,
+            getForm = function (event) {
+                event.setReturnValue(form);
+                return true;
+            },
+            formToDict = function () {
+                var fields = form.serializeArray(),
+                    json = {},
+                    i = 0;
+                for (i = 0; i < fields.length; i += 1) {
+                    json[fields[i].name] = fields[i].value;
+                }
+                return json;
+            },
+            success = function (response) {
+                self.dispatcher.notify(
+                    new Event(
+                        self,
+                        "form.response.success",
+                        {
+                            response: response.response
+                        }
+                    )
+                );
+            },
 
-    var self = this;
+            error = function (response) {
+                self.dispatcher.notify(
+                    new Event(
+                        self,
+                        "form.response.error",
+                        {
+                            response: response
+                        }
+                    )
+                );
+            },
 
-    var form = jQuery(formId || "#chat-form").bind("submit", function(e) {
-        e.stopPropagation();
-        onSubmit();
-        return false;
-    }).bind("keypress", function(e) {
-        if (e.keyCode == 13) {
+            clear = function () {
+                form.find("input[type=text]").val("").select();
+            },
+
+            onSubmit = function () {
+                var message = self.dispatcher.filter(
+                        new Event(self, "form.message.filter", {}),
+                        formToDict()
+                    ).getReturnValue(),
+                    event = self.dispatcher.notifyUntil(
+                        new Event(
+                            self,
+                            "send_message.send",
+                            {
+                                message: message,
+                                success: success,
+                                error: error
+                            }
+                        )
+                    );
+                if (!event.isProcessed()) {
+                    throw ("Event 'send_message.send' was not processed!");
+                }
+                clear();
+            };
+
+        // bind form events
+        form = jQuery(formId || "#chat-form").bind("submit", function (e) {
             e.stopPropagation();
             onSubmit();
             return false;
-        }
-    });
+        }).bind("keypress", function (e) {
+            if (13 === e.keyCode) {
+                e.stopPropagation();
+                onSubmit();
+                return false;
+            }
+        });
 
-    var getForm = function(event) {
-        event.setReturnValue(form);
-        return true;
+        // set mapping
+        this.mapping = function () {
+            return {
+                "form.get": getForm
+            };
+        };
     };
-
-    var onSubmit = function() {
-        var message = self.dispatcher.filter(
-            new Event(self, "form.message.filter", {}),
-            formToDict()
-        ).getReturnValue();
-        var event = self.dispatcher.notifyUntil(
-            new Event(self, "send_message.send",
-                {message: message, success: success, error: error}));
-        if (!event.isProcessed()) {
-            throw("Event 'send_message.send' was not processed!");
-        }
-        erase();
-    };
-
-    var success = function(response) {
-        self.dispatcher.notify(
-            new Event(self, "form.response.success",
-                {response: response.response}));
-    };
-
-    var error = function(response) {
-        self.dispatcher.notify(
-            new Event(self,
-                "form.response.error", {response: response}));
-    };
-
-    var erase = function() {
-        form.find("input[type=text]").val("").select();
-    };
-
-    var formToDict = function() {
-        var fields = form.serializeArray();
-        var json = {};
-        var i = 0;
-        for ( ; i < fields.length; i++ ) {
-            json[fields[i].name] = fields[i].value;
-        }
-        return json;
-    };
-}
+}(jQuery, Listener, Event));
