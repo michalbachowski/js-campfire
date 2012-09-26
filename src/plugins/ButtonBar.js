cef0a295ead0e6c49a5bfdc839e403c8ede19ebc
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 14:18:59 +0200

    JSLint compliance

--- src/plugins/ButtonBar.js
+++ src/plugins/ButtonBar.js
@@ -1,42 +1,45 @@
-function ChatPluginButtonBar(appendTo) {
-    Listener.apply( this, arguments );
+var ChatPluginButtonBar = (function (jQuery, Listener) {
+    "use strict";
+    return function (appendTo) {
+        Listener.apply(this, arguments);
 
-    this.mapping = function() {
-        return {
-            "chat.init": [init, 400],
-            "buttonbar.button.attach": attach
+        var className = "button-bar",
+            barTag = "<div />",
+            buttonTag = "<span />",
+            box,
+            buttons = {},
+            self = this,
+
+            wrap = function (callback) {
+                return function (e) {
+                    jQuery(this).text(callback(e));
+                };
+            },
+
+            attach = function (event) {
+                var callbacks = event.parameter("callbacks"),
+                    i,
+                    button = jQuery('<span />').text(event.parameter("label")),
+                    args = [];
+
+                for (i = 0; i < callbacks.length; i += 1) {
+                    args.push(wrap(callbacks[i]));
+                }
+                jQuery.fn.toggle.apply(button, args);
+                box.append(button);
+                return true;
+            },
+
+            init = function () {
+                box = jQuery("<div />").addClass("button-bar");
+                jQuery(appendTo || "#chat-form").append(box);
+            };
+        this.mapping = function () {
+            return {
+                "chat.init": [init, 400],
+                "buttonbar.button.attach": attach
+            };
         };
-    };
 
-    appendTo = appendTo || "#chat-form";
-    var className = "button-bar";
-    var barTag = "<div />";
-    var buttonTag = "<span />";
-    var box;
-    var buttons = {};
-    var self = this;
-
-    var wrap = function(callback) {
-        return function(e) {
-            jQuery(this).text(callback(e));
-        };
     };
-
-    var attach = function(event) {
-        var callbacks = event.parameter("callbacks");
-        var i;
-        var button = jQuery('<span />').text(event.parameter("label"));
-        var args = [];
-        for(i in callbacks) {
-            args.push(wrap(callbacks[i]));
-        }
-        jQuery.fn.toggle.apply(button, args);
-        box.append(button);
-        return true;
-    };
-
-    var init = function() {
-        box = $( "<div />" ).addClass("button-bar");
-        $(appendTo).append(box);
-    };
-}
+}(jQuery, Listener));
