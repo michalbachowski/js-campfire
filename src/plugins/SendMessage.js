8772462b8c129206bb09f113f39c6df06a678773
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 10:58:36 +0200

    JSLint compliance

--- src/plugins/SendMessage.js
+++ src/plugins/SendMessage.js
@@ -1,55 +1,54 @@
-function ChatPluginSendMessage(room, defaultUrl, timeout) {
-    Listener.apply(this, arguments);
+var ChatPluginSendMessage = (function (jQuery, Listener) {
+    "use strict";
+    return function (defaultUrl, timeout) {
+        Listener.apply(this, arguments);
 
-    timeout = timeout || 10; // seconds
-
-    this.mapping = function() {
-        return {
-            "send_message.send": send
-        };
-    };
-
-    room = room || "ognisko";
-    var self   = this;
-
-    var prepareUrl = function(eventUrl) {
-        if (eventUrl === undefined) {
-            return url;
-        }
-        return eventUrl.replace( /%room%/, room);
-    };
-    var url = prepareUrl(defaultUrl || "/jbapp/chat/%room%/odpowiedz.json");
-
-    var send = function(event) {
-        jQuery.ajax({
-            url: prepareUrl(event.parameter("url")),
-            data: event.parameter('message'),
-            dataType: "text",
-            type: "POST",
-            success: function( response ) {
-                var params = jQuery.parseJSON(response);
-                if (params.status !== 1) {
-                    invokeCallback(params, event.parameter("error"));
-                } else {
-                    invokeCallback(params, event.parameter("success"));
+        timeout = timeout || 10; // seconds
+        room = room || "ognisko";
+        var self   = this,
+            prepareUrl = function (eventUrl) {
+                if (eventUrl === void 0) {
+                    return defaultUrl;
+                }
+                return eventUrl.replace(/%room%/, room);
+            },
+            url = prepareUrl(defaultUrl || "/jbapp/chat/%room%/odpowiedz.json"),
+            invokeCallback = function (response, callback) {
+                if (callback === void 0) {
+                    return;
+                }
+                try {
+                    return callback(response);
+                } catch (e) {
+                    return;
                 }
             },
-            error: function( response ) {
-                invokeCallback(response, event.parameter("error"));
-            },
-            timeout: timeout * 1000
-        });
-        return true;
+            send = function (event) {
+                jQuery.ajax({
+                    url: prepareUrl(event.parameter("url")),
+                    data: event.parameter('message'),
+                    dataType: "text",
+                    type: "POST",
+                    success: function (response) {
+                        var params = jQuery.parseJSON(response);
+                        if (params.status !== 1) {
+                            invokeCallback(params, event.parameter("error"));
+                        } else {
+                            invokeCallback(params, event.parameter("success"));
+                        }
+                    },
+                    error: function (response) {
+                        invokeCallback(response, event.parameter("error"));
+                    },
+                    timeout: timeout * 1000
+                });
+                return true;
+            };
+     
+        this.mapping = function () {
+            return {
+                "send_message.send": send
+            };
+        };
     };
-
-    function invokeCallback(response, callback) {
-        if (callback === undefined) {
-            return;
-        }
-        try {
-            return callback(response);
-        } catch(e) {
-            return;
-        }
-    }
-}
+}(jQuery, Listener));
