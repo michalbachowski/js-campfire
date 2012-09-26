24cbe7ab5eebca4498e20d17e9cff3267c0fc5a3
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 14:53:12 +0200

    JSLint compliance

--- src/plugins/Me.js
+++ src/plugins/Me.js
@@ -1,16 +1,16 @@
-function ChatPluginMe() {
+var ChatPluginMe = function () {
+    "use strict";
     Listener.apply( this, arguments );
 
-    this.mapping = function() {
-        return {
-            "display_message.node.filter": notify
-        };
-    };
-
-    var notify = function(event, node) {
-        if ( event.parameter("message").me ) {
-            node.addClass( "me" );
+    var filter = function (event, node) {
+        if (event.parameter("message").me) {
+            node.addClass("me");
         }
         return node;
+    };
+    this.mapping = function () {
+        return {
+            "display_message.node.filter": filter
+        };
     };
 }
