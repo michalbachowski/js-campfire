ac82d6f0440edb98bd36e2f10e4ede49f6b8406b
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 15:28:15 +0200

    JSLint compliance

--- src/plugins/Nap.js
+++ src/plugins/Nap.js
@@ -1,60 +1,77 @@
-function ChatPluginNap( params ) {
-    Listener.apply(this, arguments);
+var ChatPluginNap = (function (jQuery, Listener, Event, setInterval, clearInterval) {
+    "use strict";
+    return function (params) {
+        Listener.apply(this, arguments);
 
-    this.mapping = function() {
-        return {
-            "dispatcher.message.display": [filter, 400],
-            "chat.init": init
+        var options = jQuery.extend(
+                {
+                    messageInterval: 4 * 60,    // seconds
+                    labelSleep: "take a nap",
+                    labelWakeUp: "wake up"
+                },
+                params
+            ),
+            self = this,
+            intervalTime = options.messageInterval * 1000,
+            interval,
+            send = function () {
+                self.dispatcher.notifyUntil(
+                    new Event(
+                        self,
+                        "send_message.send",
+                        {
+                            message: {
+                                message: "/nap"
+                            }
+                        }
+                    )
+                );
+            },
+            sleep = function () {
+                if (!interval) {
+                    interval = setInterval(send, intervalTime);
+                }
+                return options.labelWakeUp;
+            },
+            wakeUp = function () {
+                if (interval) {
+                    clearInterval(interval);
+                    interval = null;
+                }
+                return options.labelSleep;
+            },
+            init = function () {
+                self.dispatcher.notifyUntil(
+                    new Event(
+                        self,
+                        "buttonbar.button.attach",
+                        {
+                            label: options.labelSleep,
+                            callbacks: [
+                                sleep,
+                                wakeUp
+                            ]
+                        }
+                    )
+                );
+            },
+
+            filter = function (event) {
+                var data = event.parameter("message");
+                if (data.hasOwnProperty("nap")) {
+                    return;
+                }
+                if (Math.floor(Math.random() * 10) > 9) {
+                    return true;
+                }
+            };
+
+        this.mapping = function () {
+            return {
+                "dispatcher.message.display": [filter, 400],
+                "chat.init": init
+            };
         };
+
     };
-
-    var options = jQuery.extend(
-        {
-            messageInterval: 4 * 60,    // seconds
-            labelSleep: "take a nap",
-            labelWakeUp: "wake up"
-        },
-        params
-    );
-    var self = this;
-
-    var intervalTime = options.messageInterval * 1000;
-    var interval;
-
-    var init = function() {
-        self.dispatcher.notifyUntil(
-            new Event(self, "buttonbar.button.attach",
-                {label: options.labelSleep, callbacks: [sleep, wakeUp]}));
-    };
-
-    var filter = function(event) {
-        var data = event.parameter("message");
-        if (!("nap" in data)) {
-            return;
-        }
-        if (Math.floor( Math.random() * 10 )>9) {
-            return true;
-        }
-    };
-
-    var send = function() {
-        self.dispatcher.notifyUntil(
-            new Event(self, "send_message.send",
-                {message: {message: "/nap"}}));
-    };
-
-    var sleep = function() {
-        if ( !interval ) {
-            interval = setInterval(send, intervalTime);
-        }
-        return options.labelWakeUp;
-    };
-
-    var wakeUp = function() {
-        if ( interval ) {
-            clearInterval( interval );
-            interval = null;
-        }
-        return options.labelSleep;
-    };
-}
+}(jQuery, Listener, Event, setInterval, clearInterval));
