cc25cb0ac92ea278d294febe15576c97c1a343ad
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 15:36:28 +0200

    Initial import of example usage

---
+++ example/static/js/init.js
@@ -0,0 +1,31 @@
+var campfire = new Chat(function (callback) {
+    poller('/jbapp/chat/ognisko/rozmowa.json', callback)
+});
+
+campfire
+    .attach(new ChatPluginAuth({
+        loginUrl: '/jbapp/chat/ognisko/wejscie.json',
+        logoutUrl: '/jbapp/chat/ognisko/wyjscie.json'
+    }))
+    .attach(new ChatPluginBan())
+    .attach(new ChatPluginButtonBar())
+    .attach(new ChatPluginColor())
+    .attach(new ChatPluginDirect())
+    .attach(new ChatPluginDispatcher())
+    .attach(new ChatPluginDisplayMessage())
+    .attach(new ChatPluginForm())
+//    .attach(new ChatPluginInfoBox(jQuery("#info-box").dialog()))      // requires jQuery.dialog as input argument
+    .attach(new ChatPluginMe()) 
+    .attach(new ChatPluginMessageTime())
+    .attach(new ChatPluginNap())
+    .attach(new ChatPluginPuppet())
+    .attach(new ChatPluginSendMessage('/jbapp/chat/ognisko/odpowiedz.json'))
+    .attach(new ChatPluginTitleAlert())
+    .attach(new ChatPluginTyping())
+    .attach(new ChatPluginUrlize())
+    .attach(new ChatPluginUsersList())
+    .attach(new ChatPluginYoutubize());
+
+// DEBUG
+// ognisko.dispatcher().connect("chat.message.display",console.log, 200);
+campfire.init();
