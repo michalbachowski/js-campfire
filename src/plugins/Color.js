d65a918f4ab54238594c3d5159ea7ecd0c8b3ab1
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 14:10:57 +0200

    JSLint compliance

--- src/plugins/Color.js
+++ src/plugins/Color.js
@@ -1,24 +1,26 @@
-function ChatPluginColor( params ) {
-    Listener.apply( this, arguments );
+var ChatPluginColor = function (params) {
+    "use strict";
+    Listener.apply(this, arguments);
 
-    this.mapping = function() {
+    var colorize = function (event, node) {
+        var data = event.parameter("message");
+        if (!data.hasOwnProperty('color')) {
+            return node;
+        }
+        if (null === data.color) {
+            return node;
+        }
+        if (0 === data.color.length) {
+            return node;
+        }
+        node.css('color', data.color);
+        return node;
+    };
+    
+    this.mapping = function () {
         return {
             "display_message.node.filter": colorize
         };
     };
 
-    var colorize = function(event, node) {
-        var data = event.parameter("message");
-        if (!('color' in data)) {
-            return node;
-        }
-        if ( null === data.color ) {
-            return node;
-        }
-        if ( 0 === data.color.length ) {
-            return node;
-        }
-        node.css( 'color', data.color );
-        return node;
-    };
 }
