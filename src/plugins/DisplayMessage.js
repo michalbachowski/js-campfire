db1299cd96a117914cbd005ea7d5da1b5117432f
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 13:44:20 +0200

    JSLint compliance; Removed code that should be put into separated plugin

--- src/plugins/DisplayMessage.js
+++ src/plugins/DisplayMessage.js
@@ -1,72 +1,61 @@
-//ChatPluginDisplayMessage.prototype = new Listener();
-function ChatPluginDisplayMessage(inboxId) {
-    Listener.apply(this, arguments);
-    
-    this.mapping = function() {
-        return {
-            "dispatcher.message.display": display
+var ChatPluginDisplayMessage = (function (jQuery, Listener, Event) {
+    "use strict";
+    return function (inboxId) {
+        Listener.apply(this, arguments);
+        
+        var self   = this,
+            $inbox = jQuery(inboxId || "#inbox"),
+            createNode = function (from, displayName, message, id) {
+                return jQuery("<p />")
+                    .addClass("const")
+                    .data("chat-user", from)
+                    .attr("id", "m" + id)
+                    .attr("data-author", from.name) // CSS
+                    .hide()
+                    .append(jQuery("<strong />").append(displayName).addClass("user-name"))
+                    .append(jQuery("<span />").append(" ").append(message).addClass("message"));
+            },
+            display = function (event) {
+                var message = event.parameter("message"),
+                    userName,
+                    content,
+                    node;
+                if (message === void 0) {
+                    return;
+                }
+                if ($inbox.find("#m" + message.id).length > 0) {
+                    return;
+                }
+
+                // HOOK: prepare username
+                userName = self.dispatcher.filter(
+                    new Event(self, "display_message.name.filter", {"message": message}),
+                    message.from.name
+                ).getReturnValue();
+
+                // HOOK: prepare message
+                content = self.dispatcher.filter(
+                    new Event(self, "display_message.message.filter", {"message": message}),
+                    message.message
+                ).getReturnValue();
+
+                // HOOK: prepare node
+                node = self.dispatcher.filter(
+                    new Event(self, "display_message.node.filter", {"message": message}),
+                    createNode(message.from, userName, content, message.id)
+                ).getReturnValue();
+
+                // append node
+                $inbox.prepend(node);
+                node.slideDown("slow");
+
+                return true;
+            };
+
+        this.mapping = function () {
+            return {
+                "dispatcher.message.display": display
+            };
         };
     };
-
-    var self   = this;
-    var inbox = jQuery(inboxId || "#inbox");
-    var display = function(event) {
-        var message = event.parameter("message");
-        var priv_inbox = $('#priv_inbox');
-        if ( message === undefined ) {
-            return;
-        }
-        var existing = jQuery("#m" + message.id, inbox);
-        if (existing.length>0) {
-            return;
-        }
-
-        // HOOK: prepare username
-        var userName = self.dispatcher.filter(
-            new Event(self, "display_message.name.filter", {"message": message}),
-            message.from.name
-        ).getReturnValue();
-
-        // HOOK: prepare message
-        var content = self.dispatcher.filter(
-            new Event(self, "display_message.message.filter", {"message": message}),
-            message.message
-        ).getReturnValue();
-
-        // HOOK: prepare node
-        var node = self.dispatcher.filter(
-            new Event(self, "display_message.node.filter", {"message": message}),
-            createNode(message.from, userName, content, message.id)
-        ).getReturnValue();
-
-        // append node
-        var docelowy_box = inbox;
-        if ($.jStorage.get('privy_w_osobnej_karcie') == true) {
-            if (message.to == undefined)
-                {
-            //    docelowy_box = inbox;
-                }
-            else
-                {
-                docelowy_box = priv_inbox;
-                }
-        }
-        $('#' + docelowy_box.attr('data-tab')).trigger('new_messages');
-       
-        docelowy_box.prepend(node);
-        node.slideDown("slow");
-
-        return true;
-    };
-
-    var createNode = function(from, displayName, message, id ) {
-        return jQuery( "<p />" )
-            .addClass( "const" )
-            .data( "chat-user", from )
-            .attr( "id", "m" + id )
-            .attr( "data-author", from.name) // CSS
-            .hide()
-            .append( jQuery( "<strong />" ).append( displayName ).addClass( "user-name" ) )
-            .append( jQuery( "<span />" ).append( " " ).append( message ).addClass( "message" ) );
-    };
-}
+}(jQuery, Listener, Event));
