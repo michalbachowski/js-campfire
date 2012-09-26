e00256536baf50f5c4708a38b93c737a202480ce
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-09-26 15:31:12 +0200

    JSLint compliance

--- src/plugins/Urlize.js
+++ src/plugins/Urlize.js
@@ -1,18 +1,21 @@
-function ChatPluginUrlize() {
+var ChatPluginUrlize = function () {
+    "use strict";
     Listener.apply( this, arguments );
 
-    this.mapping = function() {
+    var urls  = new RegExp(
+            "(^|[ \\t\\r\\n])((?:(?:http(s)?://)|(www\\.))" +
+                "((?:[a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@#&~=%-]*))?" +
+                "([A-Za-z0-9$_+!*();/?:~-]))",
+            "g"
+        ),
+        tag = '$1<a href="http$3://$4$5$6" target="_blank">$2</a>',
+        urlize = function (event, msg) {
+            return msg.replace(urls, tag);
+        };
+
+    this.mapping = function () {
         return {
             "display_message.message.filter": urlize
         };
     };
-
-    var urls  = new RegExp(
-        "(^|[ \\t\\r\\n])((?:(?:http(s)?://)|(www\\.))((?:[a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@#&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))",
-        "g"
-    );
-    var tag = '$1<a href="http$3://$4$5$6" target="_blank">$2</a>';
-    var urlize = function(event, msg) {
-        return msg.replace( urls, tag );
-    };
-}
+};
