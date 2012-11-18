cf914e3f6644f663d2965e0a9efd478d7520ac8d
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-11-18 17:18:59 +0100

    Set priority for 'chat.init' handler in order to fire it AFTER Form plugin

--- src/plugins/Direct.js
+++ src/plugins/Direct.js
@@ -14,7 +14,7 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
 
         var self = this,
             options = $.extend(true, {}, defaults, params),
-            input = $(options.inputSelector),
+            $input,
 
             // append "Priv" button to each user node on users list
             filter = function (event, node) {
@@ -44,14 +44,16 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
             },
 
             init = function (event) {
+                // set input field (here, because form might not be set previously)
+                $input = $(options.inputSelector);
                 // handle clicks on "Priv" button
                 $("body").on("click", "." + options.button.className, function (e) {
                     var name = $(e.target).closest(options.userNodeSelector).get(0).dataset.nick,
-                        val = input.val();
+                        val = $input.val();
                     if (val.substr(0, 1) === '>') {
                         return false;
                     }
-                    input.focus().val('>' + name + ': ' + val);
+                    $input.focus().val('>' + name + ': ' + val);
                     return false;
                 });
             };
@@ -61,7 +63,7 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
                 "display_message.node.filter": display,
                 "users_list.node.filter": [filter, 50],
                 "display_message.name.filter": username,
-                "chat.init": init
+                "chat.init": [init, 350]
             };
         };
     };
