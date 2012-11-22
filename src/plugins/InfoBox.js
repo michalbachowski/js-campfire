15f6ceb7b93ba43358ad3fd11ab6860577b1a074
Michał - MiB - Bachowski <mib@heroes.net.pl>
2012-11-22 12:48:56 +0100

    Refactored InfoBox plugin to use Bootstrap features

--- src/plugins/InfoBox.js
+++ src/plugins/InfoBox.js
@@ -1,20 +1,74 @@
-function ChatPluginInfoBox(box) {
+var ChatPluginInfoBox = (function ($, Listener, Handlebars) {
     "use strict";
-    Listener.apply(this, arguments);
-    
-    var self = this,
-        display = function (event) {
-            box
-                .dialog("close")
-                .text(event.parameter("message"))
-                .dialog("open");
-            return true;
-        };
 
-    this.mapping = function () {
-        return {
-            "info_box.display": display
-        };
+    var defaults = {
+        template: {
+            alert: Handlebars.compile('<div class="{{className}}">' +
+                    '<button type="button" class="close" data-dismiss="alert">×</button>' +
+                    '<strong class="alert-header"></strong> <span class="alert-content"></span>' +
+                '</div>')
+        },
+        options: {
+            alert: {
+                className: 'alert'
+            },
+            info: {
+                header: 'Info',
+                content: 'message',
+                type: 'warning'
+            }
+        },
+        methods: {
+            insert: function (box) {
+                box.appendTo("#body");
+            },
+            display: function (box) {
+                box.slideDown("slow");
+            },
+            prepare: (function () {
+                var types = {
+                        'warning': '',
+                        'error': 'alert-error',
+                        'success': 'alert-success',
+                        'info': 'alert-info'
+                    },
+                    headers = {
+                        'warning': 'Warning',
+                        'error': 'Error',
+                        'success': 'Success',
+                        'info': 'Info'
+                    };
+                return function (node, data) {
+                    return node.removeClass('alert-error alert-success alert-info').addClass(types[data.type])
+                        .find(".alert-header").html(headers[data.type]).end();
+                };
+            }())
+        }
     };
 
-}
+    return function (params) {
+        Listener.apply(this, arguments);
+        
+        var self = this,
+            options = $.extend(true, {}, defaults, params),
+            $box,
+            display = function (event) {
+                var data = $.extend(true, {}, options.options.info, event.parameters());
+                $box.find(".alert-content").html(data.content).end();
+                options.methods.display(options.methods.prepare($box, data));
+                return true;
+            },
+
+            init = function (event) {
+                $box = $(options.template.alert(options.options.alert)).hide();
+                options.methods.insert($box);
+            };
+
+        this.mapping = function () {
+            return {
+                "info_box.display": display,
+                "chat.init": init
+            };
+        };
+    };
+}(jQuery, Listener, Handlebars));
