3f94fa3ca5a458ec78d7e4ed7fc948ca4b83f012
Michał Bachowski <mib@heroes.net.pl>
2013-08-27 13:59:13 +0200

    Refactored Direct plugin to track private message recipients as long as proper 'Private message' button is active

--- src/plugins/Direct.js
+++ src/plugins/Direct.js
@@ -6,7 +6,7 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
         button: {
             label: '<i class="icon icon-comment" />',
             className: 'button-direct',
-            attrs: 'rel="tooltip" data-original-title="Private message"'
+            attrs: 'rel="tooltip" data-toggle="button" data-original-title="Private message"'
         }
     };
 
@@ -15,6 +15,7 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
 
         var self = this,
             options = $.extend(true, {}, defaults, params),
+            $clickedButton = void 0,
             $input,
 
             // append "Priv" button to each user node on users list
@@ -44,21 +45,50 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
                 return username;
             },
 
+            remove_recipient = function ($input) {
+                var val = $input.val(),
+                    has_lead = val.substr(0, 1) === '>',
+                    separator_position = val.indexOf(':');
+                if (!has_lead) {
+                    return;
+                }
+                $input.val(val.substr(separator_position + 1).trimLeft());
+            },
+
+            nick_from_button = function ($button) {
+                return $button.closest(options.userNodeSelector).get(0).dataset.nick;
+            },
+
+            on_clear = function (event) {
+                if ($clickedButton === void 0) {
+                    return false;
+                }
+                var name = nick_from_button($clickedButton),
+                    val = $input.val();
+                $input.focus().val('>' + name + ': ' + val);
+            },
+
             init = function (event) {
                 // set input field (here, because form might not be set previously)
                 $input = $(options.inputSelector);
                 // handle clicks on "Priv" button
                 $("body").on("click", "." + options.button.className, function (e) {
-                    var $target = $(e.target),
-                        name = $target.closest(options.userNodeSelector).get(0).dataset.nick,
-                        val = $input.val(),
-                        has_lead = val.substr(0, 1) === '>',
-                        separator_position = val.indexOf(':');
-                    // remove current recipient
-                    if (has_lead && separator_position > 1) {
-                        val = val.substr(separator_position + 1).trimLeft();
+                    var $target = $(e.target).closest(".btn");
+                    // have active session
+                    if ($clickedButton !== void 0) {
+                        remove_recipient($input);
+                        // same button clicked - stop private session and
+                        // forget clickedButton, so new session with same user might be established
+                        if ($target.get(0) === $clickedButton.get(0)) {
+                            $clickedButton = void 0;
+                            return false;
+                        }
+                        $clickedButton.button('toggle');
                     }
-                    $input.focus().val('>' + name + ': ' + val);
+                    // other button clicked;
+                    $clickedButton = $target;
+                    // prepend message form with recipient name
+                    on_clear();
                     return false;
                 });
             };
@@ -68,6 +98,7 @@ var ChatPluginDirect = (function (Listener, $, Handlebars, Event) {
                 "display_message.node.filter": display,
                 "users_list.node.filter": [filter, 50],
                 "display_message.name.filter": username,
+                "form.message.cleared": on_clear,
                 "chat.init": [init, 350]
             };
         };
